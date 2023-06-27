4691faa9adcf6a775d56751ccb104b9968760394
Na Astou <naastou1@gmail.com>
2023-06-27 23:09:00 +0200

    suppression console.log

--- src/setDrink.js
+++ src/setDrink.js
@@ -1,10 +1,9 @@
 // récupère l'id du coktail cliqué et le stocke dnas le local storage
 
 const setDrink = (section) => {
-  console.log(section);
   section.addEventListener("click", function (e) {
     const idCocktail = e.target.parentElement.dataset.id;
-    console.log(idCocktail);
+
     localStorage.setItem("drink", idCocktail);
   });
 };
